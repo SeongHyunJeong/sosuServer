cecb1cc5758023222ccec63284aa71a4789a7486
SeongHyunJeong <sean3080211@gmail.com>
2016-12-22 18:41:08 +0900

    채팅

--- server(챗)/client.js
+++ server(챗)/client.js
@@ -1,5 +1,12 @@
 let socket = io.connect("http://localhost:9999");
 
-socket.on("connection",(socket) => {
-    console.log("서버와 연결되었습니다");
+socket.on('welcome message',(data) => {
+    document.write(data.message + '<br>');
+                    //받아옴
+
+    socket.disconnect();
 });
+
+socket.on('check alive', (data) => {
+    document.write(data.message + '<br>');
+})
--- server(챗)/index.js
+++ server(챗)/index.js
@@ -2,5 +2,18 @@ let io = require('socket.io')(9999);
 
 io.on('connection', (client) => {
   console.log("클라이언트가 열결되었습니다.");
+
+  //보내기                                 //출력할메세지
+  client.emit('welcome message', {message: "Welcome"});
+
+  client.on('disconnnect', () => {
+    console.log(`클라이언트 접속 해제: ${client.id}`)
+  });
+
 });
 
+
+
+setInterval(()=>{
+    io.emit('check alive',{message: "Helloooo"})
+}, 1000);
